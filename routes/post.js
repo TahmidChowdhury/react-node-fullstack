8d78ceb4ee4cea373bd813ec23126ee06608c822
Tahmid <tahmid.chowdhury28@gmail.com>
2019-03-07 15:41:01 -0500

    adding a user router.param

--- routes/post.js
+++ routes/post.js
@@ -2,11 +2,16 @@ const express = require('express');
 const {getPosts, createPost} = require('../controllers/post');
 const {createPostValidator} = require('../validator');
 const {requireSignin} = require('../controllers/auth');
+const {userById} = require('../controllers/user');
+
 
 const router = express.Router();
 
 router.get('/', requireSignin, getPosts);
-router.post('/post', createPostValidator, createPost);
+router.post('/post',requireSignin, createPostValidator, createPost);
+
+//any route containing :userId, our app will first execute userByID()
+router.param("userId", userById);
 
 
 module.exports = router;
