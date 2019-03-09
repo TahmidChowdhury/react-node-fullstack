9d3e36c3bb3bb3befaab951c3db3853180a98cb8
Tahmid <tahmid.chowdhury28@gmail.com>
2019-03-09 12:35:26 -0500

    added in postById, isPoster, updatePost, deletePost functionality

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,13 @@
 const express = require('express');
-const {getPosts, createPost, postsByUser} = require('../controllers/post');
+const {
+  getPosts,
+  createPost,
+  postsByUser,
+  postById,
+  isPoster,
+  deletePost,
+  updatePost
+} = require('../controllers/post');
 const {createPostValidator} = require('../validator');
 const {requireSignin} = require('../controllers/auth');
 const {userById} = require('../controllers/user');
@@ -7,12 +15,20 @@ const {userById} = require('../controllers/user');
 
 const router = express.Router();
 
-router.get('/', requireSignin, getPosts);
-router.post('/post/new/:userId',requireSignin, createPost, createPostValidator);
+router.get('/posts', requireSignin, getPosts);
+router.post(
+  '/post/new/:userId',
+  requireSignin,
+  createPost,
+  createPostValidator);
 
 router.get("/posts/by/:userId", requireSignin, postsByUser);
+router.put('/post/:postId', requireSignin, isPoster, updatePost);
+router.delete('/post/:postId', requireSignin, isPoster, deletePost);
+
 //any route containing :userId, our app will first execute userByID()
 router.param("userId", userById);
-
+//any route containing :postId, our app will first execute postByID()
+router.param("postId", postById);
 
 module.exports = router;
