0dee96baa09e8c2f2b9ab38b5da90677ee98369a
Tahmid <tahmid.chowdhury28@gmail.com>
2019-03-09 10:43:21 -0500

    added a route for postsByUser and edited user posts

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getPosts, createPost} = require('../controllers/post');
+const {getPosts, createPost, postsByUser} = require('../controllers/post');
 const {createPostValidator} = require('../validator');
 const {requireSignin} = require('../controllers/auth');
 const {userById} = require('../controllers/user');
@@ -8,8 +8,9 @@ const {userById} = require('../controllers/user');
 const router = express.Router();
 
 router.get('/', requireSignin, getPosts);
-router.post('/post',requireSignin, createPostValidator, createPost);
+router.post('/post/new/:userId',requireSignin, createPost, createPostValidator);
 
+router.get("/posts/by/:userId", requireSignin, postsByUser);
 //any route containing :userId, our app will first execute userByID()
 router.param("userId", userById);
 
